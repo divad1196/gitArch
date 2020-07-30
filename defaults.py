18a4804a1daa2bbcabae414ea3d5967b96102841
divad1196 <davidtennis96@hotmail.com>
2020-07-30 15:48:17 +0200

    first commit

---
+++ defaults.py
@@ -0,0 +1,2 @@
+DEFAULT_REGISTERY_FILENAME = "repositories_registry.json"
+DEFAULT_STATE_FILENAME = "repositories_state.json"
---
+++ test.py
@@ -0,0 +1,6 @@
+from tools import *
+from defaults import *
+
+path = Path("/home/opennet/Odoo/v12_3")
+ensure_server(DEFAULT_REGISTERY_FILENAME, DEFAULT_STATE_FILENAME, path)
+
